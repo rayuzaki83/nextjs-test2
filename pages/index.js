ea09971fd1f3f4e5aed05472f583dcd98bc048a3
rayuzaki83 <rayyap247@gmail.com>
2022-03-27 17:10:21 +0100

    Additional modifications with additional colours

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import styled from 'styled-components';
 
 export default function Home() {
   return (
@@ -7,7 +8,23 @@ export default function Home() {
         <title>My NextJS App</title>
       </Head>
 
-      <div>Hello World!</div>
+      <Wrapper>
+        <Button>Click me!</Button>
+      </Wrapper>
     </div>
   );
 }
+
+const Wrapper = styled.div`
+  padding: 4em;
+  background: papayawhip;
+`;
+
+const Button = styled.button`
+  background: #00b894;
+  border-radius: 3px;
+  border: none;
+  padding: 10px;
+  margin: 10px;
+  color: white
+  `;
