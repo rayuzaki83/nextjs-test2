d58f0b75e8e0f5ced599772ae955687aad6ce88e
rayuzaki83 <rayyap247@gmail.com>
2022-03-27 17:58:20 +0100

    css changed to Tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,18 @@
 import Head from 'next/head';
-import styled from 'styled-components';
 
 export default function Home() {
   return (
     <div>
       <Head>
         <title>My NextJS App</title>
+        <script src="https://cdn.tailwindcss.com"></script>
       </Head>
 
-      <Wrapper>
-        <Button>Click me!</Button>
-      </Wrapper>
+      <div>
+        <div className="text-3xl font-bold underline text-orange-500 ">
+          Click me again!
+        </div>
+      </div>
     </div>
   );
 }
-
-const Wrapper = styled.div`
-  padding: 4em;
-  background: papayawhip;
-`;
-
-const Button = styled.button`
-  background: #00b894;
-  border-radius: 3px;
-  border: none;
-  padding: 10px;
-  margin: 10px;
-  color: white
-  `;
